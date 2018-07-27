fb226471609ff4e7e9b2d1919cc976f7c2ce1cf3
Baylor <df_hanxusheng@163.com>
2018-07-27 17:14:00 +0800

    index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,30 +1,15 @@
 <template>
-  <div class="test">
-    <div id="signature"></div>
-    <button type="button">输出签名</button>
+  <div>
+    <router-link to="/table">table</router-link>
   </div>
 </template>
 <script>
-
 export default {
-  data () {
-    return {
-      data: 'vue'
-    }
-  },
-  methods: {
-    getCaname() {
-      console.log('相机')
-    }
-  }
+
 }
 </script>
 <style lang="scss">
 
-#signature {
-  height: 300px;
-  width: 300px;
-  background: #f5f5f5;
-}
-
 </style>
+
+
